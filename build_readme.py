465d2a63c5f4b8f95eb2e453ff9f374d383aeaf2
flloaers <florence.lenaers@gmail.com>
2023-08-20 10:24:57 +0200

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -3,8 +3,35 @@ import pathlib
 import re
 import datetime
 
-# get current date
-current_date = datetime.date.today()
+def replace_writing(content, marker, chunk, inline=False):
+    r = re.compile(
+        r'<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->'.format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = '\n{}\n'.format(chunk)
+    chunk = '<!-- {} starts -->{}<!-- {} ends -->'.format(marker, chunk, marker)
+    return r.sub(chunk, content)
 
-print(current_date)
+if __name__ == '__main__':
+    readme_path = root / 'README.md'
+    readme = readme_path.open().read()
+    entries, entry_count = fetch_writing()
+    print(f'Recent 5: {entries}, Total count: {entry_count}')
+    entries_md = '\n'.join(
+        ['* [{title}]({url}) - {published}'.format(**entry) for entry in entries]
+    )
+
+    # get current date
+    current_date = datetime.date.today()
+
+    # Update entries
+    updated_date = replace_writing(readme, 'update_date', current_date)
+    readme_path.open('w').write(updated_date)
+
+    # Update count
+    readme = readme_path.open().read()  # Need to read again with updated entries
+   
+  
+
 
