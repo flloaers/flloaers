16957d966012e879c22e90368e5b7d9fbe0ea6fc
flloaers <florence.lenaers@gmail.com>
2023-08-20 10:51:22 +0200

    Create build_readme.py

---
+++ build_readme.py
@@ -0,0 +1,34 @@
+import feedparser
+import pathlib
+import re
+import datetime
+
+root = pathlib.Path(__file__).parent.resolve()
+
+def replace_writing(content, marker, chunk, inline=False):
+    r = re.compile(
+        r'<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->'.format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = '\n{}\n'.format(chunk)
+    chunk = '<!-- {} starts -->{}<!-- {} ends -->'.format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+if __name__ == '__main__':
+    readme_path = root / 'README.md'
+    readme = readme_path.open().read()
+
+    # get current date
+    current_date = datetime.date.today()
+
+    # Update entries
+    updated_date = replace_writing(readme, 'update_date', current_date)
+    readme_path.open('w').write(updated_date)
+
+    # Update count
+    readme = readme_path.open().read()  # Need to read again with updated entries
+   
+  
+
+
